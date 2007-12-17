31d1a5c75ed5232c3a2e16f258c1812e0ef16c72
Neil Mitchell <ndmitchell@gmail.com>
2007-12-17 16:14:39 +0000

    Change the build script to be written in Haskell

---
+++ Main.hs
@@ -0,0 +1,56 @@
+
+module Main(main) where
+
+import System.Directory
+import System.FilePath
+import System.Cmd
+import System.Exit
+import Control.Exception
+import Control.Monad
+
+
+main = do
+    let obj = "obj"
+    eps <- files "graphics" "eps"
+    tex <- files "." "tex"
+    bib <- files "." "bib"
+
+    for eps $
+        \e -> replaceDirectory e obj <== e $ copyFile
+    for bib $
+        \b -> replaceDirectory b obj <== b $ \from to -> do
+            copyFile from to
+            system_ "obj" $ "bibtex " ++ takeFileName b
+    for tex $
+        \t -> replaceDirectory t obj <== t $ \from to -> do
+            system_ "." $ "lhs2tex " ++ from ++ " -o " ++ to
+
+    system_ "obj" "texify thesis.tex"
+    copyFile "obj/thesis.dvi" "thesis.dvi"
+
+
+for = flip mapM
+
+files dir ext = do
+    s <- getDirectoryContents dir
+    s <- return $ filter ((==) ('.':ext) . takeExtension) s
+    return $ map (dir </>) s
+
+
+system_ dir cmd = do
+    orig <- getCurrentDirectory
+    bracket_ (setCurrentDirectory dir) (setCurrentDirectory orig) $ do
+        putStrLn cmd
+        res <- system cmd
+        when (res /= ExitSuccess) $ error "System command failed!"
+
+(<==) :: FilePath -> FilePath -> (FilePath -> FilePath -> IO ()) -> IO ()
+(<==) to from action = do
+    b <- doesFileExist to
+    rebuild <- if not b then return True else do
+        from2 <- getModificationTime from
+        to2 <- getModificationTime to
+        return $ to2 < from2
+    when rebuild $ do
+        putStrLn $ "Building: " ++ to
+        action from to
--- make.bat
+++ make.bat
@@ -1,19 +1,3 @@
 mkdir obj
-
-REM Graphics
-copy graphics\*.eps obj
-
-REM Tex files
-for %%i in (*.tex) do lhs2tex %%i -o obj\%%i
-
-REM Bibtex
-copy thesis.bib obj
-
-REM Combine
-cd obj
-bibtex thesis
-texify thesis.tex %1
-cd ..
-
-del thesis.dvi
-copy obj\thesis.dvi thesis.dvi
+ghc --make Main.hs -odir obj -hidir obj
+main.exe
