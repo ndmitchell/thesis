e77175b35c3eaf5d78af5969170723611a827233
Neil Mitchell <ndmitchell@gmail.com>
2007-12-22 13:52:17 +0000

    Make all the .tex files depend on thesis.fmt

--- Main.hs
+++ Main.hs
@@ -16,13 +16,13 @@ main = do
     bib <- files "." "bib"
 
     for eps $
-        \e -> replaceDirectory e obj <== e $ copyFile
+        \e -> replaceDirectory e obj <== [e] $ copyFile
     for bib $
-        \b -> replaceDirectory b obj <== b $ \from to -> do
+        \b -> replaceDirectory b obj <== [b] $ \from to -> do
             copyFile from to
             system_ "obj" $ "bibtex " ++ takeFileName b
     for tex $
-        \t -> replaceDirectory t obj <== t $ \from to -> do
+        \t -> replaceDirectory t obj <== [t,"thesis.fmt"] $ \from to -> do
             system_ "." $ "lhs2tex " ++ from ++ " -o " ++ to
 
     system_ "obj" "texify thesis.tex"
@@ -44,11 +44,11 @@ system_ dir cmd = do
         res <- system cmd
         when (res /= ExitSuccess) $ error "System command failed!"
 
-(<==) :: FilePath -> FilePath -> (FilePath -> FilePath -> IO ()) -> IO ()
-(<==) to from action = do
+(<==) :: FilePath -> [FilePath] -> (FilePath -> FilePath -> IO ()) -> IO ()
+(<==) to froms@(from:_) action = do
     b <- doesFileExist to
     rebuild <- if not b then return True else do
-        from2 <- getModificationTime from
+        from2 <- liftM maximum $ mapM getModificationTime froms
         to2 <- getModificationTime to
         return $ to2 < from2
     when rebuild $ do
