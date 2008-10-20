56a5b49ccdcc07fa89cbe429030921b84314bdef
Neil Mitchell <ndmitchell@gmail.com>
2008-10-20 16:16:44 +0000

    Rework the index check to go direct from \ind, rather than the generated index

--- IndexCheck.hs
+++ IndexCheck.hs
@@ -1,6 +1,7 @@
 
 module Main where
 
+import Control.Arrow
 import Control.Monad
 import Data.Char
 import Data.List
@@ -13,27 +14,42 @@ boring = ["import","hiding","(",")",",","module","->","[","]"
          ,"data","`","::","if","then","else","where","}"
          ,"{","let","in","case","of","...","..","do"]
          ++
-         ["alpha","beta"]
+         ["alpha","beta"] ++ words extra
+
+extra = unwords $
+  ["While CoreData CoreExpr Core CoreFunc CoreVar CoreApp CoreCase CoreLet UniplateExpr uniplateExpr ns"
+  ,"childrenExpr universeExpr descendExpr transformExpr plateListDiff res"
+  ,"asTypeOf cast gmapQ gmapM isAlgType dtyp ctrs fromConstr dataTypeConstrs"
+  ,"dataTypeOf uni_variables syb_variables everything mkQ com_variables"
+  ,"uni_zeroCount syb_zeroCount com_zeroCount simp uni_simplify syb_simplify everywhere"
+  ,"mkT com_simplify Stm SDecl Typ SAss Exp SBlock SReturn EStm EAdd EInt V T_int T_float"
+  ,"ren uni_rename syb_rename com_rename uni_symbols syb_symbols com_symbols optimise"
+  ,"uni_constFold syb_constFold com_constFold Manager Employee Address Company C Dept D PU DU E"
+  ,"Person Salary P S incS uni_increase syb_increase com_increase uni_incrOne descendBi syb_incrOne"
+  ,"isDept gmapT isDeptD com_incrOne uni_salaryBill syb_salaryBill billS com_salaryBill"]
+  
 
 
 main = do
-    index <- readIndex "obj/make/thesis.ind"
     files <- getDirectoryContents "."
-    files <- return $ filter ((==) ".tex" . takeExtension) files
-    mapM_ (checkTex index) files
+    (index,used) <- liftM concatUnzip $ mapM readTex $ filter ((==) ".tex" . takeExtension) files
+    let bad = nub used \\ (boring ++ index)
+    when (bad /= []) $
+        putStrLn $ unwords bad
 
 
-readIndex file = do
-    src <- readFile file
-    return [takeWhile (/= ',') $ drop 8 x | x <- lines src, "  \\item " `isPrefixOf` x]
+concatUnzip = (concat *** concat) . unzip
 
 
+-- (index,code)
+readTex :: FilePath -> IO ([String],[String])
 readTex file = do
     src <- readFile file
-    return $ concatMap f $ tails src
+    return $ concatUnzip $ map f $ tails src
     where
-        f xs | "\\begin{code}" `isPrefixOf` xs = readCode $ scanEnd $ drop 12 xs
-             | otherwise = []
+        f xs | "\\begin{code}" `isPrefixOf` xs = ([],readCode $ scanEnd $ drop 12 xs)
+             | "\\ind{" `isPrefixOf` xs = ([takeWhile (/= '}') $ drop 5 xs],[])
+             | otherwise = ([],[])
 
 
 scanEnd xs | "\\end{code}" `isPrefixOf` xs = []
@@ -47,11 +63,3 @@ readCode xs = case lex $ dropWhile isSpace xs of
                   [(x,y)] -> [x | good x] ++ readCode y
     where
         good (x:xs) = isAlpha x -- not $ x `elem` "'\"" || isDigit x
-
-
-checkTex index file = do
-    putStrLn $ "Checking index for " ++ file
-    used <- readTex file
-    let bad = nub used \\ (boring ++ index)
-    when (bad /= []) $
-        putStrLn $ "Bad index items in " ++ file ++ ": " ++ unwords bad
